dcd08e459541cd651a5cf7fa84d45d04c8bc0a57
gourikam <gourikamakhija25@gmail.com>
2024-07-19 12:24:39 +0530

    Scripts

---
+++ Scripts/ClearCounter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearCounter : BaseCounter
+{
+
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            //there is no kitchenobject here
+            if (player.HasKitchenObject())
+            {
+                //player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else
+            {
+                //player not carrying anything
+            }
+        }
+        else
+        {
+            //there is a kitchenobject here
+            if (player.HasKitchenObject())
+            {
+                // player is carrying something
+            } else
+            {
+                //player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+    
+
+}
